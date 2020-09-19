ba1c03a6fab7b6ecbccccfbfc859eeb6287154c5
agivins <70180293+agivins@users.noreply.github.com>
2020-09-18 18:55:46 -0700

    Add files via upload

---
+++ Practices 13&14 (1).py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[73]:
+
+
+import pandas as pd
+import numpy as np
+import glob
+
+
+# In[8]:
+
+
+all_data = pd.DataFrame()
+for f in glob.glob("C:/Users/Admin/Desktop/datasets/weekdata*.xlsx"):
+    df = pd.read_excel(f)
+    all_data = all_data.append(df,ignore_index=True)
+all_data.describe()
+
+
+# In[10]:
+
+
+df.head()
+
+
+# In[75]:
+
+
+import pandas as pd
+from sqlalchemy import create_engine
+# Connect to sqlite db
+db_file = r'C:/Users/Admin/Desktop/datasets/gradedata.db'
+engine = create_engine(r"sqlite:///{}"
+                       .format(db_file))
+sql = 'SELECT * from test where Grades in (76,77,78)'
+sales_data_df = pd.read_sql(sql, engine)
+sales_data_df
+
+
+# In[76]:
+
+
+sql = "select name from sqlite_master"
+"where type = 'table';"
+
+
+# In[78]:
+
+
+sql = "select * from test;"
+
+
+# In[84]:
+
+
+sales_data_df.head()
+
+
+# In[ ]:
+
+
+
+
+
+# In[87]:
+
+
+
+
+
+# In[2]:
+
+
+import pandas as pd
+import numpy as np
+import glob
+all_data = pd.DataFrame()
+for f in glob.glob("C:/Users/Admin/Desktop/datasets/weekdata*.xlsx"):
+    df = pd.read_excel(f)
+    all_data = all_data.append(df,ignore_index=True)
+all_data.describe()
+
+
+# In[5]:
+
+
+import pandas as pd
+from sqlalchemy import create_engine
+# Connect to sqlite db
+db_file = r'C:/Users/Admin/Desktop/datasets/gradedata.db'
+engine = create_engine(r"sqlite:///{}"
+                       .format(db_file))
+sql = 'SELECT * from test where Grades in (76,77,78)'
+sales_data_df = pd.read_sql(sql, engine)
+sales_data_df
+
+
+# In[6]:
+
+
+
+
+
+# In[7]:
+
+
+sql = "select name from sqlite_master"
+"where type = 'table';"
+
+
+# In[11]:
+
+
+sql = "select * from test;"
+
+
+# In[12]:
+
+
+sales_data_df.head() 
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
